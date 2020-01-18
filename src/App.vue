8b6c5e2b21d3a92360128e0f204b422e0f6c7824
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-18 11:13:42 +0200

    Custom link example

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@
       <h1 slot="quoteTitle">{{ quoteTitleVal }}</h1>
       <p slot="quoteText">{{ quoteTextVal }}</p>
     </Quote>
+    <br />
+
+    <my-link url="http://google.com"><i class="fa fa-user" /> Link Text</my-link>
+
   </div>
 </template>
 
 <script>
 import Quote from "@/components/Quote";
+import MyLink from "@/components/MyLink";
 
 export default {
   data() {
@@ -19,6 +24,7 @@ export default {
   },
   components: {
     Quote,
+    myLink: MyLink,
   },
 }
 </script>
---
+++ src/components/MyLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <a :href="url"><slot /></a>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
