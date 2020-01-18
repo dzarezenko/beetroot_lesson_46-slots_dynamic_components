fb4f8669ff323547474d8ca97fd0e1a504db3529
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-18 11:56:04 +0200

    Dynamic Components

---
+++ src/UserApp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button @click="currentComponent = 'UserPhoto'">User Photo</button> |
+    <button @click="currentComponent = 'UserContacts'">User Contacts</button> |
+    <button @click="currentComponent = 'UserDetails'">User Details</button>
+    <p>
+      {{ currentComponent }}
+    </p>
+    <hr />
+
+    <component :is="currentComponent"></component>
+
+  </div>
+</template>
+
+<script>
+import UserPhoto from "@/components/user/UserPhoto";
+import UserContacts from "@/components/user/UserContacts";
+import UserDetails from "@/components/user/UserDetails";
+
+export default {
+  data() {
+    return {
+      currentComponent: "UserPhoto",
+    }
+  },
+  components: {
+    UserPhoto, UserContacts, UserDetails
+  },
+}
+</script>
---
+++ src/components/user/UserContacts.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>User Contacts</h1>
+</template>
